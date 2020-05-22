23d2fa6e4de53eb695c3eecadbc1f4c430b63ac8
Gregg Mudhuwiwa <gnice5@gmail.com>
2020-05-22 13:34:18 +0800

    Assgn 3 moved map functions to drawPupil function

--- Assignment3/sketch.js
+++ Assignment3/sketch.js
@@ -1,9 +1,10 @@
 var centerx, centery; centerEye; centerR; i, centerL; distX, distY;  //variables to hold the center xy 
-var mapX;
+var mapX, mapY, centerMapX;
 function setup() {
 createCanvas(1024, 768); 
 centery = height/2;
-centerx = width/2; 	
+centerx = width/2;
+ 	
 }
 function draw() {
 background(255);
@@ -14,13 +15,14 @@ for (i = -200; i < 300;i+=400 ) {  //(draw eyes + 2 concentric pupils)* 2
 	centerEye = centerx + i; //x coord for left and right eyes
 	
 	
-/*This part of the code figures out where the mouse eyes for drawing pupils*/
+/*This part of the code figures out where the mouse is for drawing pupils*/
+		stroke(0);
 	
-	stroke(0);
+deltaY(); //map mouse Y position to pupil position
 
-	if ((i<0) && (mouseX < centerEye)) {//mouse is to the left of left Eye
+	if ((i<0) && (mouseX < centerEye)) {//mouse is to the left of left Eye and drawing left eye
 		distX = mouseX - centerEye; //how far away is the mouse
-		mapX = map(distX, -centerEye, 0,  (centerEye - 80), centerEye); //mapping to control moved eye	
+		//mapX = map(distX, -centerEye, 0,  (centerEye - 80), centerEye); //mapping to control moved eye	
 		
 		line(pmouseX, pmouseY, centerEye, centery); 
 		for (var y = 0; y<401; y+=400) {  //draw both pupils for mouse to left
@@ -32,12 +34,15 @@ for (i = -200; i < 300;i+=400 ) {  //(draw eyes + 2 concentric pupils)* 2
 	}
 	else if((mouseX>centerx-200) &&(mouseX < centerx+200)) {  //between the eyes
 		line(pmouseX, pmouseY, centerEye, centery); 
-		stroke(0);
+		centerMapX = map(mouseX, centerx-200, centerx+200, 0, 400); //where is the mouse in the gap
+		//mapX=map(centerMapX, 0, 400, 0, 90 ); //map the gap to pupil position within the eye 
+		//drawPupils();
+		//noFill();
 	}//between eyes
 	
-	else if ((i>0) && (mouseX > centerEye)) {//mouse is to the right of the right eye
+	else if ((i>0) && (mouseX > centerEye)) {//mouse is to the right of the right eye a
 		distX = mouseX - centerEye; //how far away is the mouse
-		mapX = map(distX, centerEye, 0,  (centerEye + 90), centerEye); //mapping to control moved eye	
+		//mapX = map(distX, centerEye, 0,  (centerEye + 90), centerEye); //mapping to control moved eye	
 		ellipse(mapX, centery, 30, 30);
 		line(pmouseX, pmouseY, centerEye, centery); 
 		
@@ -51,9 +56,7 @@ for (i = -200; i < 300;i+=400 ) {  //(draw eyes + 2 concentric pupils)* 2
 		
 	ellipse(centerEye, centery, 250, 180);  //2 ellipses 250w * 180 h
 	
-	
-	
-	
+	//drawPupils(); 	
 	noFill();  //stop filling heterchomatic colours
 	stroke(0); //return stroke
 	
@@ -63,20 +66,32 @@ for (i = -200; i < 300;i+=400 ) {  //(draw eyes + 2 concentric pupils)* 2
 }
 
 function drawPupils(){
-	ellipse(mapX, centery, 80, 80);
+	ellipse(mapX, mapY, 80, 80);
 	//noStroke()  //draw the inner circle in colour
-	text("here", centerEye, centery);
+	text(mapX, centerEye, mapY);  //left eye
 	if (i==-200) { //draw the heterochrome effect - left eye
-		
+		mapX = map(distX, -centerEye, 0,  (centerEye - 80), centerEye); //mapping to control moved eye	
 		fill(102, 255, 0);  //bright green fill
-		ellipse(mapX, centery, 30, 30);
+		ellipse(mapX, mapY, 30, 30);
 	
 	}
 	else if(i == 200) {
+		mapX = map(distX, centerEye, 0,  (centerEye + 90), centerEye); //mapping to control moved eye	
 		fill(65, 105, 225); //bright blue fill
-		ellipse(mapX, centery, 30, 30);
+		ellipse(mapX, mapY, 30, 30);
 		
 	}
+}
+function deltaY() {
+	if (mouseY<centery) {  //mouse above center
+		mapY = map(mouseY, 0, centery, centery -60, centery);  //map to moving the pixes within 60px
+	} 
+	else if(mouseY>centery) { //under the centery line
+		mapY = map(mouseY, centery, height, centery, centery+60 );//move pupil 60 below the center
+		
+	}
+	else {}
+	
 }
 
 

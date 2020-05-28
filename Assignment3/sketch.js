78f3d314592787ae72079f467b90ea7a8df5ca66
Gregg Mudhuwiwa <gnice5@gmail.com>
2020-05-28 14:49:47 +0800

    limited outer for loop to just drawing static ellipes. The rest of the code is run off if depending on where the mouse is. Now about to the fix the center to work with if AND the convergence issue

--- Assignment3/sketch.js
+++ Assignment3/sketch.js
@@ -4,28 +4,32 @@ function setup() {
 createCanvas(1024, 768); 
 centery = height/2;
 centerx = width/2;
+centerL = centerx-200; //center of the left eye is 200px to the left of center
+centerR = centerx+200; //center of the right eye is 200px to the right of center
  	
 }
 function draw() {
 background(255);
 noFill(); //make everything transparent
 
-//using 2 for loops to draw 3 ellipses for each eye with global var counter i
+//using 2 for loops to draw 2 outer, static elipses 200px to the left and right of center
 for (i = -200; i < 300;i+=400 ) {  //(draw eyes + 2 concentric pupils)* 2
 	centerEye = centerx + i; //x coord for left and right eyes
+	ellipse(centerEye, centery, 250, 180);  //2 ellipses 250w * 180 h
 	
-	
+	}
 /*This part of the code figures out where the mouse is for drawing pupils*/
 		stroke(0);
 	
 deltaY(); //map mouse Y position to pupil position
 
-	if ((i<0) && (mouseX < centerEye)) {//mouse is to the left of left Eye and drawing left eye
-		distX = mouseX - centerEye; //how far away is the mouse from left eye center
-		mapX = map(distX, -centerEye, 0,  (centerEye - 80), centerEye); //mapping the range of mouseX on the left of the eye
+	if ((mouseX < centerL)) {//mouse is to the left of left Eye and drawing left eye
+		distX = mouseX - centerL; //how far away is the mouse from left eye center
+		mapX = map(distX, -centerL, 0,  (centerL - 80), centerL); //mapping the range of mouseX on the left of the eye
 		//to range move movement of pupil within the eye (80)
-		
-		line(pmouseX, pmouseY, centerEye, centery); //testing line
+		i=-200;//set i to -200 which causes the left pupil to be drawn in drawPupil() we then draw the right pupil
+		//by adding 400 to i at the bottom of this for loop
+		line(pmouseX, pmouseY, centerL, centery); //testing line
 		for (var y = 0; y<401; y+=400) {  //draw both pupils for mouse to left of left eye
 			mapX+=y	//offset 400 to move right pupil to the same relative position in its eye
 			drawPupils(); //function defined below 2 modularise drawing the pupils
@@ -50,18 +54,19 @@ deltaY(); //map mouse Y position to pupil position
 		
 	}//between eyes
 	
-	else if ((i>0) && (mouseX > centerEye)) {//mouse is to the right of the right eye a
-		distX = mouseX - centerEye; //how far away is the mouse
-		mapX = map(distX, centerEye, 0,  (centerEye + 90), centerEye); //mapping the range of mouseX on the left of the eye
+	else if ((mouseX > centerR)) {//mouse is to the right of the right eye a
+		distX = mouseX - centerR; //how far away is the mouse
+		mapX = map(distX, centerR, 0,  (centerR + 90), centerR); //mapping the range of mouseX on the left of the eye
 		//to range move movement of pupil within the eye (80)		
 		//ellipse(mapX, centery, 30, 30);  //testing ellipse
-		line(pmouseX, pmouseY, centerEye, centery); 
-		
+		line(pmouseX, pmouseY, centerR, centery); 
+		i=200// init the for loop counter so that the right then left pupils get drawn when drawPupils is called below
+		//this is done by subtracting 400 from i which prompts the 2nd iteration to draw the left eye 
 		for (var x = 0; x<401; x+=400) {  //draw both pupils
 			mapX-=x	//offset 400 between drawing to draw left pupil in the same relative position in that eye
 			drawPupils(); //function defined below 2 modularise drawing the pupils
 			noFill();//stop filling
-			//i-=400; //incrementing the loop counter causes the other pupil to be drawn heterchromatic in the 2nd iteration of
+			i-=400; //incrementing the loop counter causes the other pupil to be drawn heterchromatic in the 2nd iteration of
 			//this for loop AND stops the second iteration of the parent for loop
 			
 			
@@ -69,13 +74,13 @@ deltaY(); //map mouse Y position to pupil position
 		
 	}
 		
-	ellipse(centerEye, centery, 250, 180);  //2 ellipses 250w * 180 h
+	
 	
 	//drawPupils(); 	
 	noFill();  //stop filling heterchomatic colours
 	stroke(0); //return stroke
 	
-}
+
 
 	
 }

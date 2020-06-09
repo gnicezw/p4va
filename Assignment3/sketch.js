1303badfb8a2e5a625ea720581bd12e0eaeb88c7
Gregg Mudhuwiwa <gnice5@gmail.com>
2020-06-09 19:53:23 +0800

    Update before changing the map function from right eye movement to correct the bounds of mapX so that the constrainedcMouseX works properly

--- Assignment3/sketch.js
+++ Assignment3/sketch.js
@@ -1,16 +1,20 @@
 var centerx, centery; centerEye; centerR; i, centerL; distX, distY;  //variables to hold the center xy 
-var mapX, mapY, centerMapX, tempmapX;
+var mapX, mapY, centerMapX, tempmapX, cMouseX, cMouseY;
+var boolMotion = false; //bool value set by "training"
+
 function setup() {
 createCanvas(1024, 768); 
 centery = height/2;
 centerx = width/2;
 centerL = centerx-200; //center of the left eye is 200px to the left of center
-centerR = centerx+200; //center of the right eye is 200px to the right of center
+centerR = centerx+200; //center of the right eye is 200px to the right of cente
+
  	
 }
 function draw() {
 background(255);
 noFill(); //make everything transparent
+
 
 //using 2 for loops to draw 2 outer, static elipses 200px to the left and right of center
 for (i = -200; i < 300;i+=400 ) {  //(draw eyes + 2 concentric pupils)* 2
@@ -20,12 +24,21 @@ for (i = -200; i < 300;i+=400 ) {  //(draw eyes + 2 concentric pupils)* 2
 	}
 /*This part of the code figures out where the mouse is for drawing pupils*/
 		stroke(0);
-	
+boolMotion = true;
+//This if is currently not working as boolMotio being evaluated as false no matter what I do. 
+if (boolMotion) { //boolean value to turn on eye controls after certain amount of training
+		cMouseX = constrain(mouseX, 0, width); //constrain mouseX to canvas
+		cMouseY = constrain(mouseY, 0, height); //constrain mouseY to canvas
+	}
+else {
+		cMouseX = mouseX;
+		cMouseY = mouseY;
+	}
 deltaY(); //map mouse Y position to pupil position
 
-	if ((mouseX < centerL)) {//mouse is to the left of left Eye and drawing left eye
-		distX = mouseX - centerL; //how far away is the mouse from left eye center
-		mapX = map(distX, -centerL, 0,  (centerL - 80), centerL); //mapping the range of mouseX on the left of the eye
+	if ((cMouseX < centerL)) {//mouse is to the left of left Eye and drawing left eye
+		distX = cMouseX - centerL; //how far away is the mouse from left eye center
+		mapX = map(distX, -centerL, 0,  (centerL - 80), centerL); //mapping the range of cMouseX on the left of the eye
 		//to range move movement of pupil within the eye (80)
 		i=-200;//set i to -200 which causes the left pupil to be drawn in drawPupil() we then draw the right pupil
 		//by adding 400 to i at the bottom of this for loop
@@ -39,9 +52,9 @@ deltaY(); //map mouse Y position to pupil position
 		}
 	
 	}
-	else if((mouseX>centerx-200) &&(mouseX < centerx+200)) {  //between the eyes
+	else if((cMouseX>centerx-200) &&(cMouseX < centerx+200)) {  //between the eyes
 		line(pmouseX, pmouseY, centerEye, centery); //testing line between the eyes
-		centerMapX = map(mouseX, centerL, centerR, 0, 400); //map mouse position with center to 0 --> 400 range
+		centerMapX = map(cMouseX, centerL, centerR, 0, 400); //map mouse position with center to 0 --> 400 range
 		if (centerMapX <=200) { //if we're to the left or on center of the center-zone 
 			mapX = map(centerMapX, 0, 200, 0, 90 ); //the close the mouse is to center the more converged the pupils should be 
 			//direct mapping because we want maximum pupil displacement when mouse is at center (200)
@@ -55,14 +68,14 @@ deltaY(); //map mouse Y position to pupil position
 		tempmapX = mapX; //holds mapX between drawing iterations so it can be reset after the first for() changes it
 		for (i; i<300; i+=400) {
 			if (i==-200) { //when we're drawing the left pupil it should diplace from centerL towards the right
-				text(mapX, mouseX, mapY);
+				text(mapX, cMouseX, mapY);
 				mapX +=centerL; //mapX is large as the mouse approaches center  so we add it to center L
 								
 			} 
 		
 			else if(i==200) {  //when we draw the right pupil, it should displace for centerR towards the left
 				mapX = tempmapX; //the first for() changes mapX to displace the left pupil we reset it here
-				text(mapX, mouseX, mapY+20);
+				text(mapX, cMouseX, mapY+20);
 				mapX = (centerR - mapX); //mapX is larger as mouse approaches center so we subtract is from centerR	
 				
 			}
@@ -71,8 +84,8 @@ deltaY(); //map mouse Y position to pupil position
 		}   
 	}//between eyes
 	
-	else if ((mouseX > centerR)) {//mouse is to the right of the right eye a
-		distX = mouseX - centerR; //how far away is the mouse
+	else if ((cMouseX > centerR)) {//mouse is to the right of the right eye a
+		distX = cMouseX - centerR; //how far away is the mouse
 		mapX = map(distX, centerR, 0,  (centerR + 90), centerR); //mapping the range of mouseX on the left of the eye
 		//to range move movement of pupil within the eye (80)		
 		//ellipse(mapX, centery, 30, 30);  //testing ellipse
@@ -119,11 +132,11 @@ function drawPupils(){  //draw left eye
 	}
 }
 function deltaY() {
-	if (mouseY<centery) {  //mouse above center
-		mapY = map(mouseY, 0, centery, centery -60, centery);  //map to moving the pixes within 60px
+	if (cMouseY<centery) {  //mouse above center
+		mapY = map(cMouseY, 0, centery, centery -60, centery);  //map to moving the pixes within 60px
 	} 
-	else if(mouseY>centery) { //under the centery line
-		mapY = map(mouseY, centery, height, centery, centery+60 );//move pupil 60 below the center
+	else if(cMouseY>centery) { //under the centery line
+		mapY = map(cMouseY, centery, height, centery, centery+60 );//move pupil 60 below the center
 		
 	}
 	else {}

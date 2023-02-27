5b161801f1d8402b3ad8b370b1a94e7226fc9c83
gnicezw <gnice5@gmail.com>
2023-02-27 10:13:05 +0800

    Final working code with all 4 quadrants implemented and commented.

--- Mouse Circle Task/sketch.js
+++ Mouse Circle Task/sketch.js
@@ -10,14 +10,25 @@ function draw() {
   background(220);  //set color gray
   ellipse(mouseX, mouseY, Circle_width, Circle_height); //draw a circle at the mouse pointer location
   //make an if case statement to control circle size by location
-  if ((mouseX<width/2) && (mouseY<height/2))    {
-        Circle_width = mouseX; 
-        Circle_height = mouseX;
+  if ((mouseX<width/2) && (mouseY<height/2))    {  //top left quadrant
+        Circle_width = (mouseX+mouseY)/2; 
+        Circle_height = (mouseX+mouseY)/2;
   }
-  else {
-        Circle_width = 10; 
-        Circle_height = 10;
+  else if ((mouseX>width/2) && (mouseY<height/2)) {  //top right quadrant
+          Circle_width = ((500-mouseX)+mouseY)/2;   //x is now greater than 500 so invert it by
+          Circle_height = ((500-mouseX)+mouseY)/2;  //subtracting it from
+
     }
+    else if ((mouseX<width/2) && (mouseY>height/2)) {  //bottom left quadrant
+          Circle_width = (mouseX+(500 - mouseY))/2;   //y is now greater than 500 so invert it by
+          Circle_height = (mouseX+(500 - mouseY))/2;  //subtracting from 500
+      
+    } 
+      else if ((mouseX>width/2) && (mouseY>height/2)) {  //bottom right quadrant
+              Circle_width = ((500-mouseX)+(500 - mouseY))/2;   //y and x are now greater than 500 so invert them by
+              Circle_height = ((500-mouseX)+(500 - mouseY))/2;  //subracting from 500
+      } 
+    
   }
 
   
